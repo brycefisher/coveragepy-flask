870f5e2e6d0f111279482ed6e9e1cbc2b9862a0b
Bryce Fisher-Fleig <brycefisher@users.noreply.github.com>
2019-06-24 17:14:15 -0700

    Working locally with docker -- try again in CircleCI

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
+import sys
+import signal
 from flask import Flask
+
+
+def sigterm_handler(*args):
+    [print(a) for a in args]
+    sys.exit()
+
+
+signal.signal(signal.SIGTERM, sigterm_handler)
+
 app = Flask(__name__)
 
 
